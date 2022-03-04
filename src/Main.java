5f945a8edb11b437eccb6d08a3f35daa0615ff85
Kris Penn <kristophermp2001@gmail.com>
2022-03-04 14:45:34 -0500

    Created new cardgame project. Use package csc439team3.cardgame for Main class. Created Main class with main method with appropriate JavaDocs. Created test folder.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main class contains main method which will simulate the play of a card game.
+ */
+public class Main {
+    /**
+     * Used to simulate the process of a card game.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
